73238ba64e1cb67ab4b9cdea39121c008e1ecc85
Atul Veer <atul.veer@gmail.com>
2016-07-12 19:19:53 +0530

    Show subjects as per User who added them

--- App/Components/AllSubjectsView.js
+++ App/Components/AllSubjectsView.js
@@ -9,7 +9,6 @@ const styles = require('../styles.js')
 const ESStyles = require('../ESStyles.js')
 const StatusBar = require('./StatusBar');
 const constants = styles.constants;
-const Firebase = require('firebase');
 import {observer} from 'mobx-react/native'
 
 
@@ -29,93 +28,67 @@ var {
 
 var Utility = require('NativeModules');
 
-var config = {
-  apiKey: 'AIzaSyDjYb5qDEsGtYJnYvldohcSHZtq-zOnpRc',
-  authDomain: 'prana-4be90',
-  databaseURL: 'https://prana-4be90.firebaseio.com',
-  storageBucket: 'prana-4be90.appspot.com'
-};
 
-Firebase.initializeApp(config);
-
-// Get a reference to the database service
-var database = Firebase.database();
-
-
-@observer
+//@observer
 class AllSubjectsView extends Component{
 
   constructor(props) {
     super(props);
-    this.authenticateFirebase()
-    var ds = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2, });  
+    databaseRef = this.props.databaseRef
+    var ds = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2, });
     this.state = {
       dataSource:  ds,
-      subjectsRef: database.ref("Subjects"),
+      subjectsRef: databaseRef.ref("Subjects"),
       activeSubject: "",
       activeSwitch: false,
       activityInprogress: true,
     }
-   
+
   }
 
- authenticateFirebase = () => {
-  console.log("Authenticating firebase")
-  Firebase.auth().signInAnonymously().catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    console.log(errorMessage)
-
-  });
-
-  // TODO Find a better way to do this.
-
-  var thisRef = this
-
-
-  Firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        console.log("Anno. user id:")
-        console.log(uid)
-        thisRef.listenForSubjects();
-        return true
-
-      } else {
-        console.log("Anno. user is NOT logged in")
-        return false
-      }
-
-    });
-
- }
-
-      
 
  listenForSubjects = () => {
       console.log('fetching subjects')
-   
-      var subjects = [];
-      var thisRef = this
 
+      var subjects = [];
+      var thisRef = this;
+
+// All subjects
+
+/*
     	this.state.subjectsRef.on('value', function(snap) {
-      			// get children as an array
 			      snap.forEach((child) => {
 			        subjects.push({
 			          subjectName: child.val().firstName + " " + child.val().lastName,
 			          _key: child.key
 			        });
-			      });  
-        thisRef.loadSubjects(subjects)    
+			      });
+        thisRef.loadSubjects(subjects)
     });
+*/
+
+
+// Subjects of Current user.
+
+     uid = this.props.store.currentUser.uid
+     console.log(uid)
+    this.state.subjectsRef.orderByChild('uid').equalTo(uid).on('value', function( snapshot ){
+          snapshot.forEach((child) => {
+            subjects.push({
+              subjectName: child.val().firstName + " " + child.val().lastName,
+              _key: child.key
+            });
+          });
+        thisRef.loadSubjects(subjects)
+    });
+
+
 }
 
 loadSubjects = (subjects) => {
-  console.log('loading subjcts')
-  console.log(subjects)  
-   
+  //console.log('loading subjcts')
+  console.log(subjects)
+
    this.setState({
       dataSource: this.state.dataSource.cloneWithRows(subjects),
       activityInprogress: false
@@ -123,8 +96,7 @@ loadSubjects = (subjects) => {
 }
 
   componentDidMount = () =>  {
-
-    
+    this.listenForSubjects();
   }
 
   _handlePress = () =>  {
@@ -133,25 +105,25 @@ loadSubjects = (subjects) => {
 
  renderSubject = (item) =>  {
 
-      const onPress = () => {      
+      const onPress = () => {
         ReactNative.NativeModules.Utility.setActiveSubject(item._key);
         this.props.store.setActiveSubject( item.subjectName , item._key)
-        this.setState({activeSubject: item._key});          
+        this.setState({activeSubject: item._key});
       };
 
       const onSwitchChange = () => {
-        ReactNative.NativeModules.Utility.setActiveSubject(item._key);       
+        ReactNative.NativeModules.Utility.setActiveSubject(item._key);
         this.props.store.setActiveSubject( item.subjectName  , item._key)
-        this.setState({activeSubject: item._key});          
+        this.setState({activeSubject: item._key});
        };
- 
+
 
     return (
-     
-        <ListItem item={item} 
-                  onPress={onPress} 
-                  onSwitchChange={onSwitchChange} 
-                  activeSubject={this.state.activeSubject}/>
+
+        <ListItem item={item}
+                  onPress={onPress}
+                  onSwitchChange={onSwitchChange}
+                  activeSubject={this.state.activeSubject} />
     );
   }
 
@@ -162,24 +134,24 @@ loadSubjects = (subjects) => {
                                                                   style={ESStyles.activityIndicator}
                                                                   animating={this.state.activityInprogress}
                                                                   size={'small'}
-                                                                  color={'black'}/>   
+                                                                  color={'black'}/>
                                                           : null;
     return (
-        
-       <View style={styles.container}>            
+
+       <View style={styles.container}>
           <View style={styles.statusBar}>
                   <TouchableHighlight
                     underlayColor={'#FFFF'}
                     onPress={this.props.closeDrawer}>
-                       <Text style={styles.backButton}>  
+                       <Text style={styles.backButton}>
                        <Image
                             style={styles.icon}
-                            source={require('../Images/menu-bars.png')}/> 
+                            source={require('../Images/menu-bars.png')}/>
                         </Text>
-                        
+
                   </TouchableHighlight>
                   <Text style={styles.statusBarTitle}> Prana - Subjects. </Text>
-          </View>        
+          </View>
 
               {activityIndicator}
 
@@ -194,8 +166,8 @@ loadSubjects = (subjects) => {
                     onPress={this._handlePress}>
                        <Text style={styles.actionText}>Add New Subject</Text>
                   </TouchableHighlight>
-             </View>       
-             
+             </View>
+
 
         </View>
     );
@@ -203,4 +175,3 @@ loadSubjects = (subjects) => {
 };
 
 module.exports = AllSubjectsView;
-
--- App/Components/Report.js
+++ App/Components/Report.js
@@ -12,13 +12,138 @@ const { StyleSheet,
 import Communications from 'react-native-communications';
 import {observer} from 'mobx-react/native'
 
-@observer
+//@observer
 class Report extends React.Component {
 
-  emailReport = () => {
 
-    
-    Communications.email(['atulveer@gmail.com'],null,null,'My Subject','My body text')
+  constructor(props){
+    super(props)        
+    this.getSubjects()    
+
+    this.state = {
+        subjectName: "",
+        reportStartTime: "",
+        reportEndTime: "",
+        reportDuration: "",
+        leftNostrilCount: "",
+        rightNostrilCount: "",
+        activeNadi: ""
+     }
+
+  }
+
+  getSubjects() {
+
+    var subjectKey =  this.props.store.activeSubject.key;
+    var databaseRef = this.props.databaseRef
+    var breathReadings = [];
+    var thisRef = this
+    var startTime = this.props.store.reportTime.startTime
+    var endTime = this.props.store.reportTime.endTime
+    console.log("Start end endTime ")
+    console.log(startTime)
+    console.log(endTime)
+    //subjectKey = "-KKiXfayfFEiM5eANhTT"
+    //startTime = 1466832596.267107
+    //endTime =   1466832631.358009 //1466832634.433123
+    databaseRef.ref("BreathReadings").orderByChild("readingDateTime")
+                                     .startAt(startTime)
+                                     .endAt(endTime)
+                                     .on('value', function(snapshot) {                                                         
+                                               snapshot.forEach((child) => {                                               
+                                                if(subjectKey == child.val().subjectKey){
+                                                   breathReadings.push({
+                                                      activeNadi: child.val().activeNadi,
+                                                      activeTatva: child.val().activeTatva,
+                                                      exhalationDirection: child.val().exhalationDirection,                                                      
+                                                      leftTop: child.val().leftTop,
+                                                      centerTop: child.val().centerTop,
+                                                      rightTop: child.val().rightTop,
+                                                      leftBottom: child.val().leftBottom,
+                                                      leftMiddle: child.val().leftMiddle,
+                                                      rightMiddle: child.val().rightMiddle,
+                                                      rightBottom: child.val().rightBottom,
+                                                      readingDateTime: child.val().readingDateTime,
+                                                      subjectKey: child.val().subjectKey,
+                                                      key: child.key
+                                                   });
+                                                  }  
+                                              });
+                                            thisRef.generateReport(breathReadings,startTime,endTime) 
+                                   
+                         });
+   }
+
+ generateReport(breathReadings,startTime,endTime){
+
+      var reportTimeData = this.getReportDuration(startTime,endTime)
+      reportStartTime = reportTimeData[0]
+      reportEndTime = reportTimeData[1]
+      reportDuration = reportTimeData[2]
+      
+      reportData = this.processBreathReadings(breathReadings)
+      leftNostrilCount = reportData[0]
+      rightNostrilCount = reportData[1]
+      activeNadi = reportData[2]
+       
+      this.setState({
+          subjectName: this.props.store.activeSubject.fullName,
+          reportStartTime: reportStartTime,
+          reportEndTime: reportEndTime,
+          reportDuration: reportDuration,
+          leftNostrilCount: leftNostrilCount,
+          rightNostrilCount: rightNostrilCount,
+          activeNadi: activeNadi
+      })
+  }
+
+  getReportDuration(startTime,endTime){
+     var reportStartTime = new Date(0); 
+     reportStartTime.setUTCSeconds(startTime);
+
+     var reportEndTime = new Date(0); 
+     reportEndTime.setUTCSeconds(endTime);
+
+     var duration = Math.abs(reportEndTime.getTime() - reportStartTime.getTime());
+     var diffMins = Math.ceil(duration / (1000 * 60));
+     
+     return [reportStartTime,reportEndTime,diffMins]
+  }
+
+  processBreathReadings (breathReadings){
+   var leftNostrilCount = 0
+   var rightNostrilCount = 0
+   var activeNadi = ""
+
+   for(var i = 0; i< breathReadings.length; i++){
+     
+     if (breathReadings[i].activeNadi == "Ida"){
+        leftNostrilCount += 1
+     }else if(breathReadings[i].activeNadi == "Pingala")
+       rightNostrilCount += 1
+    }
+
+    if(leftNostrilCount >= rightNostrilCount){
+         activeNadi = "Ida"
+    }else {
+        activeNadi = "Pingala"
+    }
+      return [leftNostrilCount,rightNostrilCount, activeNadi]
+  }
+
+  emailReport = () => {
+    subjectBody = "Prana Report for: " + this.state.subjectName.toString()
+
+    reportBody = "<b>Report For:" + this.state.subjectName.toString() + "<br /></b>"
+    reportBody = reportBody += "From : " + this.state.reportStartTime.toString() + "<br />"
+    reportBody = reportBody += "To : " + this.state.reportEndTime.toString() + "<br />"
+    reportBody = reportBody += "Duration : " + this.state.reportDuration.toString() + " mins.<br />"
+    reportBody = reportBody += "Left Nostril Count : " + this.state.leftNostrilCount.toString() + "<br />"
+    reportBody = reportBody += "Right Nostril Count : " + this.state.rightNostrilCount.toString() + "<br />"
+    reportBody = reportBody += "Active Nadi : " + this.state.activeNadi.toString() + "<br />"
+
+    console.log(reportBody)
+    Communications.email(null,null,["atul@revelealinghour.in"],subjectBody,reportBody)
     //alert('Emailing report')
   }
 
@@ -26,22 +151,15 @@ class Report extends React.Component {
      this.props.navigator.pop(); 
   }
 
-
-  render = () => {
-    console.log(this.props.store.activeSubject)
-    activeSubjectName = this.props.store.activeSubject.fullName
-
+  render = () => {   
     return (
       <View style={styles.container}>               
   
           <View style={styles.statusBar}>
-              <TouchableHighlight
-                    underlayColor={'#FFFF'}
-                    onPress={ this._renderBreathChart}>
-                  <Text style={styles.backButton}>&lt; </Text>
-                  </TouchableHighlight>
-              
-                  <Text style={styles.statusBarTitle}>Report of {activeSubjectName} </Text>
+              <TouchableHighlight underlayColor={'#FFFF'}  onPress={ this._renderBreathChart}>
+                    <Text style={styles.backButton}> &lt; </Text>
+              </TouchableHighlight>              
+                  <Text style={styles.statusBarTitle}>Report of {this.state.subjectName} </Text>
         </View>      
          
  
@@ -50,8 +168,32 @@ class Report extends React.Component {
             rejectResponderTermination={false}
             style={ESStyles.scrollView}>
 
+              <Text style={styles.titleText}> From: 
+                  <Text style={styles.normalText}>{this.state.reportStartTime.toString()}</Text>
+              </Text>    
+
+              <Text style={styles.titleText}> To: 
+                   <Text style={styles.normalText}>{this.state.reportEndTime.toString()}</Text>
+              </Text> 
+
+              <Text style={styles.titleText}> Duration: 
+                  <Text style={styles.normalText}>{this.state.reportDuration.toString()} mins. </Text>
+              </Text> 
+
+              <Text style={styles.titleText}> Left Nostril Count: 
+                  <Text style={styles.normalText}>{this.state.leftNostrilCount.toString()}</Text>
+              </Text>     
+
+              <Text style={styles.titleText}> Right Nostril Count:
+                  <Text style={styles.normalText}>{this.state.rightNostrilCount.toString()}</Text>
+               </Text> 
+
+              <Text style={styles.titleText}> Active Nadi:  
+                     <Text style={styles.normalText}> {this.state.activeNadi.toString()} </Text>
+              </Text>
+                            
               <TouchableHighlight style={styles.button} onPress={this.emailReport} underlayColor='#99d9f4'>
-                <Text style={styles.buttonText}>Email Report</Text>
+                <Text style={styles.buttonText}>Email Report </Text>
               </TouchableHighlight>
 
         </ScrollView>   
